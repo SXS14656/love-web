38cb869c3767db35e317a62f79249ea06506fb3d
SXS14656 <55308872+SXS14656@users.noreply.github.com>
2026-07-07 23:20:03 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const pageConfig = {
-  birthday: { date: "2026-07-28" },
+  birthday: { date: "2026-07-08" },
   birthdaySong: "assets/music/happy-birthday.mp3",
   giftCard: {
     unlockCode: "0226",
